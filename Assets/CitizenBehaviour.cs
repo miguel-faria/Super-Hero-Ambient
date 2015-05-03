9f3a081f85c5bda2008c28479c32ec63b8659088
Joana Teixeira <joanbteixeira@gmail.com>
2015-05-03 19:06:57 +0100

    Bichos vão mudando de destino aleatoriamente Codigo muito incompleto com cenas por fazer, nao mexer please :)

---
+++ Assets/CitizenBehaviour.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CitizenBehaviour : MonoBehaviour {
+	
+	// Use this for initialization
+	Animator anim;
+	NavMeshAgent agent;
+	AnimatorStateInfo state;
+	Transform[] destinations;
+	float time;
+	float fieldOfViewAngle = 110f;
+
+	//public delegate void CollisionAction();
+	//public event CollisionAction onCollision;
+
+	bool isFleeing;
+	float fleeTime;
+	
+	void Start () 
+	{
+		GameObject[] objects = GameObject.FindGameObjectsWithTag("Destination");
+		destinations = new Transform[objects.Length];
+		for (int i = 0; i < objects.Length; i++)
+			destinations [i] = objects [i].transform ;
+		
+		anim = GetComponent<Animator> ();
+		agent = GetComponent<NavMeshAgent>();
+		
+		time = Time.time;
+		agent.SetDestination(destinations[Random.Range (0, destinations.Length)].position);
+		Debug.Log (agent.destination);
+		anim.SetBool ("isWalking", true);
+		state = anim.GetCurrentAnimatorStateInfo (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		state = anim.GetCurrentAnimatorStateInfo (0);
+		//Debug.Log(state.IsName("idle"));
+		//Debug.Log(agent.destination.Equals(GameObject.Find("Dest1").transform.position));
+
+		if (state.IsName("idle")) {
+			agent.Resume();
+			agent.SetDestination(destinations[Random.Range (0, destinations.Length)].position);
+			anim.SetBool("isWalking",true);
+			return;
+		}
+
+		if (state.IsName ("walk") && Time.time - time >= 1) {
+			agent.SetDestination (destinations [Random.Range (0, destinations.Length)].position);
+			Debug.Log (agent.destination);
+			anim.SetBool ("isWalking", true);
+			time = Time.time;
+			return;
+		}
+		
+		
+	}
+	
+	/*void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("seen a villain");
+		// If the player has entered the trigger sphere...
+		if (other.gameObject.CompareTag ("Villain")) {
+			// Create a vector from the enemy to the player and store the angle between it and forward.
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle (direction, transform.forward);
+			
+			// If the angle between forward and where the player is, is less than half the angle of view...
+			if (angle < fieldOfViewAngle * 0.5f) {
+				Debug.Log ("Villain colision");
+				isFleeing = true;
+
+
+
+				if(onCollision != null)
+					onCollision();
+				agent.SetDestination (other.gameObject.transform.position);
+				return;
+				
+			}
+		}
+	}*/
+}
--- Assets/GOTOscript.cs
+++ Assets/GOTOscript.cs
@@ -7,15 +7,23 @@ public class GOTOscript : MonoBehaviour {
 	Animator anim;
 	NavMeshAgent agent;
 	AnimatorStateInfo state;
-	//Transform *destinations;
-
+	Transform[] destinations;
+	float time;
+	float fieldOfViewAngle = 110f;           // Number of degrees, centred on forward, for the enemy see.
 
 	void Start () 
 	{
-		//destinations = GameObject.Find ("Destinations").GetComponentsInChildren (Transform);
+		GameObject[] objects = GameObject.FindGameObjectsWithTag("Destination");
+		destinations = new Transform[objects.Length];
+		for (int i = 0; i < objects.Length; i++)
+			destinations [i] = objects [i].transform ;
+
 		anim = GetComponent<Animator> ();
 		agent = GetComponent<NavMeshAgent>();
-		//agent.SetDestination(destinations[1].position);
+
+		time = Time.time;
+		agent.SetDestination(destinations[Random.Range (0, destinations.Length)].position);
+		Debug.Log (agent.destination);
 		anim.SetBool ("isWalking", true);
 		state = anim.GetCurrentAnimatorStateInfo (0);
 	}
@@ -26,7 +34,7 @@ public class GOTOscript : MonoBehaviour {
 
 		state = anim.GetCurrentAnimatorStateInfo (0);
 		//Debug.Log(state.IsName("idle"));
-		Debug.Log(agent.destination.Equals(GameObject.Find("exit").transform.position));
+		//Debug.Log(agent.destination.Equals(GameObject.Find("Dest1").transform.position));
 		if (Input.GetKeyDown ("space")) {
 			agent.Stop();
 			anim.SetTrigger ("Laugh");
@@ -36,16 +44,38 @@ public class GOTOscript : MonoBehaviour {
 
 		if (state.IsName("idle")) {
 			agent.Resume();
-			agent.SetDestination(GameObject.Find("exit").transform.position);
+			agent.SetDestination(destinations[Random.Range (0, destinations.Length)].position);
 			anim.SetBool("isWalking",true);
 			return;
 		}
 
-		if (state.IsName("walk") && agent.destination.Equals(GameObject.Find("exit").transform.position)) {
-			anim.SetBool("isWalking",false);
+		if (state.IsName ("walk") && Time.time - time >= 5) {
+			agent.SetDestination (destinations [Random.Range (0, destinations.Length)].position);
+			Debug.Log (agent.destination);
+			anim.SetBool ("isWalking", true);
+			time = Time.time;
 			return;
 		}
 
 
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("triggered");
+		// If the player has entered the trigger sphere...
+		if (other.gameObject.CompareTag ("Citizen")) {
+			// Create a vector from the enemy to the player and store the angle between it and forward.
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle (direction, transform.forward);
+			
+			// If the angle between forward and where the player is, is less than half the angle of view...
+			if (angle < fieldOfViewAngle * 0.5f) {
+				Debug.Log ("Citizen colision");
+				agent.SetDestination (other.gameObject.transform.position);
+				return;
+				
+			}
+		}
 	}
 }
