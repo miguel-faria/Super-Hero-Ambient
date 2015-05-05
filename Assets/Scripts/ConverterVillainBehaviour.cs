b3c5afd8760ffa6d455bbe5c1d757d8bfe30cb5e
miguel-faria <miguel.afonso.faria@gmail.com>
2015-05-06 00:11:09 +0100

    Recommit of the scripts

---
+++ Assets/Scripts/ConverterVillainBehaviour.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ConverterVillainBehaviour : MonoBehaviour
+{
+	public Slider health;
+	public Text outputConvertedCitizens;
+	public Text outputRemainingCitizens;
+	public int startingLife;
+	int remainingCitizens;
+	int convertedCitizens;
+	int life;
+	GameObject hero;
+	HeroBehaviour heroBehaviour;
+
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Time.timeScale = 1;
+		life = startingLife;
+		convertedCitizens = 0;
+		hero = GameObject.FindGameObjectWithTag ("Hero");
+		heroBehaviour = hero.GetComponent<HeroBehaviour> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		remainingCitizens = int.Parse (outputRemainingCitizens.text);
+	}
+}
+
