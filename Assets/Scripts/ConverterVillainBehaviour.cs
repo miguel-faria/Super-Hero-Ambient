008a03a1e22b37d4a38733f27fff3cb82fc13076
miguel-faria <miguel.afonso.faria@gmail.com>
2015-05-08 01:01:40 +0100

    Main Behaviour cycle for the converter villain done.

--- Assets/Scripts/ConverterVillainBehaviour.cs
+++ Assets/Scripts/ConverterVillainBehaviour.cs
@@ -1,7 +1,238 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
+//Utility classes
+class Belief{
+
+	string _type;
+
+	public Belief(string type){
+		_type = type;
+	}
+
+
+	public string Type {
+		get {
+			return _type;
+		}
+		set {
+			_type = value;
+		}
+	}
+}
+
+class SeeCitizenBelief : Belief{
+
+	GameObject _citizen;
+	Vector3 _citizenPosition;
+
+	public SeeCitizenBelief(GameObject citizen) : base ("Saw Citizen"){
+		_citizen = citizen;
+		_citizenPosition = citizen.transform.position;
+	}
+
+	public GameObject Citizen {
+		get {
+			return _citizen;
+		}
+		set {
+			_citizen = value;
+		}
+	}
+
+	public Vector3 CitizenPosition {
+		get {
+			return _citizenPosition;
+		}
+		set {
+			_citizenPosition = value;
+		}
+	}
+}
+
+class HearScreamBelief : Belief{
+
+	GameObject _screamer;
+	Vector3 _screamOrigin;
+
+	public HearScreamBelief (GameObject screamer, Vector3 screamOrigin) : base("Heard a Scream") {
+		_screamer = screamer;
+		_screamOrigin = screamOrigin;
+	}
+
+	public GameObject Screamer {
+		get {
+			return _screamer;
+		}
+		set {
+			_screamer = value;
+		}
+	}
+
+	public Vector3 ScreamOrigin {
+		get {
+			return _screamOrigin;
+		}
+		set {
+			_screamOrigin = value;
+		}
+	}
+}
+
+class SeeHeroBelief : Belief {
+
+	GameObject _hero;
+	Vector3 _heroPosition;
+
+	SeeHeroBelief (GameObject hero): base("Saw the Hero"){
+
+		_hero = hero;
+		_heroPosition = hero.transform.position;
+	}
+
+	public GameObject Hero {
+		get {
+			return _hero;
+		}
+		set {
+			_hero = value;
+		}
+	}
+
+	public Vector3 HeroPosition {
+		get {
+			return _heroPosition;
+		}
+		set {
+			_heroPosition = value;
+		}
+	}
+}
+
+class TouchHeroBelief : Belief {
+	
+	GameObject _hero;
+	Vector3 _heroPosition;
+	
+	TouchHeroBelief (GameObject hero): base("Touch the Hero"){
+		
+		_hero = hero;
+		_heroPosition = hero.transform.position;
+	}
+	
+	public GameObject Hero {
+		get {
+			return _hero;
+		}
+		set {
+			_hero = value;
+		}
+	}
+	
+	public Vector3 HeroPosition {
+		get {
+			return _heroPosition;
+		}
+		set {
+			_heroPosition = value;
+		}
+	}
+}
+
+class Desire{
+
+	string _desireName;
+	GameObject _subjectObject;
+	Vector3 _objectiveDestination;
+
+	Desire(string desireName, GameObject desireObject){
+		_desireName = desireName;
+		_subjectObject = desireObject;
+		_objectiveDestination = desireObject.transform.position;
+	}
+
+	public string DesireName {
+		get {
+			return _desireName;
+		}
+		set {
+			_desireName = value;
+		}
+	}
+	
+	public GameObject SubjectObject {
+		get {
+			return _subjectObject;
+		}
+		set {
+			_subjectObject = value;
+		}
+	}
+
+	public Vector3 ObjectiveDestination {
+		get {
+			return _objectiveDestination;
+		}
+		set {
+			_objectiveDestination = value;
+		}
+	}
+}
+
+class Intention {
+
+	string _description;
+	GameObject _intentObject;
+	bool _concluded;
+	bool _possible;
+
+	public Intention(string description, GameObject intentObject){
+		_description = description;
+		_intentObject = intentObject;
+		_concluded = false;
+		_possible = true;
+	}
+
+	public string Description {
+		get {
+			return _description;
+		}
+		set {
+			_description = value;
+		}
+	}
+
+	public GameObject IntentObject {
+		get {
+			return _intentObject;
+		}
+		set {
+			_intentObject = value;
+		}
+	}
+
+	public bool Concluded {
+		get {
+			return _concluded;
+		}
+		set {
+			_concluded = value;
+		}
+	}
+
+	public bool Possible {
+		get {
+			return _possible;
+		}
+		set {
+			_possible = value;
+		}
+	}
+}
+
+//Behaviour Class
 public class ConverterVillainBehaviour : MonoBehaviour
 {
 	const float TASKFOCUSTIME = 10f;
@@ -28,37 +259,37 @@ public class ConverterVillainBehaviour : MonoBehaviour
 	NavMeshAgent agent;
 	AnimatorStateInfo state;
 	Transform[] destinations;
-	
+
+	List<Belief> beliefs = new List<Belief>();
+	List<Desire> desires = new List<Desire>();
+	Intention intention;
+
 	// Use this for initialization
 	void Start ()
 	{
 		Time.timeScale = 1;
 		life = startingLife;
+		health.value = startingLife;
 		isAlive = true;
 		inCombat = false;
 		convertedCitizens = 0;
-		//hero = GameObject.FindGameObjectWithTag ("Hero");
-		//heroBehaviour = hero.GetComponent<HeroBehaviour> ();
+		outputConvertedCitizens.text = "" + convertedCitizens;
+		hero = GameObject.FindGameObjectWithTag ("Hero");
+		heroBehaviour = hero.GetComponent<HeroBehaviour> ();
 		anim = GetComponent<Animator> ();
 		agent = GetComponent<NavMeshAgent>();
 
 		GameObject[] objects = GameObject.FindGameObjectsWithTag("Destination");
-		GameObject[] citizens = GameObject.FindGameObjectsWithTag("Citizen");
 		int objLength = objects.Length;
-		int citizensLength = citizens.Length;
-		destinations = new Transform[objLength + citizensLength/* + 1*/];
+		destinations = new Transform[objLength];
 		for (int i = 0; i < objLength; i++)
 			destinations [i] = objects [i].transform ;
 
-		for (int i = 0; i < citizensLength; i++)
-			destinations [objLength + i] = citizens[i].transform ;
-
-		//destinations [objLength + citizensLength] = GameObject.FindGameObjectWithTag ("Hero").transform;
 		float min_dist = float.MaxValue;
 		int index = 0;
 
 		for (int i = 0; i < destinations.Length; i++) {
-			if (destinations [i].position.magnitude < min_dist) {
+			if (destinations[i] != null && destinations [i].position.magnitude < min_dist) {
 				index = i;
 				min_dist = destinations [i].position.magnitude;
 			}
@@ -66,6 +297,7 @@ public class ConverterVillainBehaviour : MonoBehaviour
 
 		followedObject = destinations [index].gameObject;
 		agent.SetDestination (followedObject.transform.position);
+		intention = new Intention ("Move Randomly", followedObject);
 		isFollowing = true;
 		Debug.Log ("Villain: " + agent.destination);
 		anim.SetBool ("isWalking", true);
@@ -78,7 +310,17 @@ public class ConverterVillainBehaviour : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		//remainingCitizens = int.Parse (outputRemainingCitizens.text);
+		remainingCitizens = int.Parse (outputRemainingCitizens.text);
+		beliefs = updateBeliefs(beliefs);
+
+		if(intention.Possible && !intention.Concluded && ((Time.time - lastDecisionTime) < TASKFOCUSTIME)){
+			executeIntention(intention);
+		}
+		else {
+			desires = updateDesires(beliefs, desires);
+			intention = updateIntention(beliefs, desires, intention);
+			executeIntention(intention);
+		}
 
 		updateAnimation ();
 	}
@@ -109,6 +351,31 @@ public class ConverterVillainBehaviour : MonoBehaviour
 			time = Time.time;
 			return;
 		}
+	}
+
+	void executeIntention(Intention intention){
+		Debug.Log ("Executing Intention: " + intention.Description);
+	}
+
+	List<Belief> updateBeliefs(List<Belief> oldBeliefs){
+		Debug.Log ("Updating Beliefs List");
+		List<Belief> newBeliefs = new List<Belief> (oldBeliefs);
+
+		return newBeliefs;
+	}
+
+	List<Desire> updateDesires(List<Belief> beliefs, List<Desire> oldDesires){
+		Debug.Log ("Updating Desires List");
+		List<Desire> newDesires = new List<Desire> (oldDesires);
+
+		return newDesires;
+	}
+
+	Intention updateIntention(List<Belief> beliefs, List<Desire> desires, Intention oldIntention){
+		Debug.Log ("Updating Intention");
+		Intention newIntention = oldIntention;
+
+		return newIntention;
 	}
 }
 
--- Assets/Scripts/HeroBehaviour.cs
+++ Assets/Scripts/HeroBehaviour.cs
@@ -1,11 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HeroBehaviour : MonoBehaviour {
 
+	const float TASKFOCUSTIME = 10f;
+	
+	public Slider health;
+	public Text outputSavedCitizens;
+	public Text outputRemainingCitizens;
+	public int startingLife;
+	public int startingCitizens;
+	int remainingCitizens;
+	int savedCitizens;
+	int life;
+	float lastDecisionTime;
+	float time;
+	float fieldOfViewAngle = 110f;           // Number of degrees, centred on forward, for the enemy see.
+	bool isFollowing;
+	bool inCombat;
+	bool isAlive;
+	
+	GameObject followedObject;
+	GameObject hero;
+	HeroBehaviour heroBehaviour;
+	
+	Animator anim;
+	NavMeshAgent agent;
+	AnimatorStateInfo state;
+	Transform[] destinations;
+
 	// Use this for initialization
 	void Start () {
 	
+		outputRemainingCitizens.text = "" + startingCitizens;
+		health.value = startingLife;
+		life = startingLife;
+		remainingCitizens = startingCitizens;
+		savedCitizens = 0;
+		outputSavedCitizens.text = "" + savedCitizens;
+
 	}
 	
 	// Update is called once per frame
